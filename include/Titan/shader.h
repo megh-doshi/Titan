842beafa8ce3be8d2ea2c42cb1617b9233dc14f2
megh-doshi <doshimegh1@gmail.com>
2022-12-06 01:27:07 -0600

    downgraded to earlier version of library

---
+++ include/Titan/shader.h
@@ -0,0 +1,18 @@
+//
+//  shader.hpp
+//  CUDA Physics
+//
+//  Created by Jacob Austin on 5/13/18.
+//  Copyright © 2018 Jacob Austin. All rights reserved.
+//
+
+#ifndef shader_hpp
+#define shader_hpp
+
+namespace titan {
+
+GLuint LoadShaders();
+
+} // namespace titan
+
+#endif /* shader_hpp */
